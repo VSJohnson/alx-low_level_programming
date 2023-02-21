9393dda271f3e880298d859993c34a39f0b27fd1
VSJohnson <victorsamuelj285@gmail.com>
2023-02-21 15:07:21 -0800

    Hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - decimal
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
