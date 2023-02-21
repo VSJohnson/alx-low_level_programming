5877dfb29f2182f45c2f171e8d4ae9453b71ff2e
VSJohnson <victorsamuelj285@gmail.com>
2023-02-21 08:36:38 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - i love numbers
+ *
+ * Return: 0 on succes
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+		return (0);
+}
